f56dda3a4234f0ee51434a0ffaa87eda31236372
LobeliaComputing <31212444+ibuninngu@users.noreply.github.com>
2024-02-17 19:28:00 +0900

    _

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # bluefox-vscode-server
+
+```bash
+> code --install-extension BlueFoxServer-0.1.0.vsix
+```
--- extension.js
+++ extension.js
@@ -2,20 +2,21 @@ const vscode = require("vscode");
 const http = require("http");
 const ws = require("ws");
 const fs = require("fs");
-// const path = require("path");
 const url = require("url");
 const glob = require("glob");
 const opn = require("opn");
+const crypto = require("crypto");
 
 const log = console.log;
 
-class UI {
-	// https://code.visualstudio.com/api/references/icons-in-labels
-	constructor() {
+class State {
+	constructor(context) {
 		this.statusBarItem;
+		this.context = context;
 	}
 
 	init() {
+		this.context.globalState.setKeysForSync(["server.online"]);
 		this.statusBarItem = Object.assign(
 			vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right, 100),
 			{
@@ -23,6 +24,14 @@ class UI {
 				command: "BlueFoxServer.OnLine",
 			}
 		);
+		this.offLine();
+		// if (this.context.globalState.get("server.online")) {
+		// 	this.statusBarItem.text = "^.,.^ BlueFox is OnLine in other window.";
+		// 	this.statusBarItem.command = null;
+
+		// } else {
+		// 	this.offLine();
+		// }
 		this.statusBarItem.show();
 	}
 
@@ -30,23 +39,139 @@ class UI {
 		this.statusBarItem.dispose();
 	}
 
+	onLine() {
+		this.statusBarItem.text = "^.,.^ BlueFox is OnLine.";
+		this.statusBarItem.command = "BlueFoxServer.OffLine";
+		this.context.globalState.update("server.online", true);
+	}
+
+	offLine() {
+		this.statusBarItem.text = "-.,.^ BlueFox is OffLine.";
+		this.statusBarItem.command = "BlueFoxServer.OnLine";
+		this.context.globalState.update("server.online", false);
+	}
+}
+
+class Gate {
+	constructor(context) {
+		this.context = context;
+		this.httpServer;
+		this.webSocketServer;
+
+		this.webSocketGate;
+		this.workspaceClients = {};
+
+	}
+
+	start() {
+		this.API = {
+			"/GetWorkspace.get": async (query, response) => {
+				let R = [];
+				for (let uuid of Object.keys(this.workspaceClients)) {
+					R.push(
+						await this.workspaceClients[uuid].send(
+							{
+								type: "GetWorkspace"
+							}
+						)
+					);
+				}
+				response.writeHead(200, {
+					"Content-Type": "application/json"
+				});
+				response.end(JSON.stringify(R), "utf-8");
+			},
+			"/GetFile.get": async (query, response) => {
+				try {
+					query = JSON.parse(query);
+					let filepath = (await this.workspaceClients[query.id].send(
+						Object.assign(
+							query,
+							{
+								type: "GetFilePath"
+							}
+						)
+					)).filePath;
+					let R = fs.readFileSync(filepath, "utf-8");
+					response.writeHead(200, { "Content-Type": "text/javascript" });
+					response.end(R, "utf-8");
+				} catch (e) {
+					response.writeHead(200, { "Content-Type": "text/javascript" });
+					response.end(`window.alert("^.,.^ Error !");`, "utf-8");
+				}
+			}
+		};
+		this.httpServer = http.createServer((request, response) => {
+			if (url.parse(request.url).pathname in this.API) {
+				this.API[url.parse(request.url).pathname](decodeURI(url.parse(request.url).query), response);
+
+			} else {
+				fs.readFile(this.context.asAbsolutePath("/media/index.html"), "utf-8",
+					(error, content) => {
+						response.writeHead(200, { "Content-Type": "text/html" });
+						response.end(content, "utf-8");
+					});
+			}
+		}).listen(7777);
+
+		this.webSocketServer = new ws.Server({ port: 8888 });
+
+		this.webSocketGate = new ws.Server({ port: 8887 });
+		this.webSocketGate.on("connection", async (webSocket) => {
+			webSocket.id = crypto.randomUUID();
+			this.workspaceClients[webSocket.id] = {
+				webSocket: webSocket,
+				messagePool: {},
+				send: async (message) => {
+					this.workspaceClients[webSocket.id].webSocket.send(
+						JSON.stringify(Object.assign(message, { id: webSocket.id }))
+					);
+					let R = new Promise((resolve, reject) => {
+						this.workspaceClients[webSocket.id].messagePool[webSocket.id] = (_) => {
+							resolve(_);
+						};
+					});
+					return R;
+				}
+			};
+			webSocket.on("message", async (message) => {
+				let data = JSON.parse(message);
+				if (Object.keys(this.workspaceClients[webSocket.id].messagePool).includes(data.id)) {
+					await this.workspaceClients[webSocket.id].messagePool[data.id](data);
+					delete this.workspaceClients[webSocket.id].messagePool[data.id];
+				}
+			});
+			webSocket.on("close", () => {
+				delete this.workspaceClients[webSocket.id];
+			});
+		});
+	}
+
+	stop() {
+		this.httpServer.close();
+		this.webSocketServer.close();
+	}
 }
 
 class Server {
 	constructor(context) {
 		this.context = context;
-		this.httpServer;
-		this.webSocketServer;
+		this.webSocketClient;
+		this.id = null;
 		this.OnLine = false;
-	}
 
-	start() {
 		this.API = {
-			"/getFileTree.get": (query, response) => {
-				let workspaceItems = vscode.workspace.workspaceFolders.map((workspaceFolder) => {
+			"SetID": (data) => {
+				this.id = data.id;
+			},
+			"GetID": (data) => {
+				return this.id;
+			},
+			"GetWorkspace": (data) => {
+				let workspace = vscode.workspace.workspaceFolders.map((workspaceFolder) => {
 					return {
-						workspace: workspaceFolder.name,
-						files: glob.sync(`${workspaceFolder.uri.path.slice(1)}/**/*.js`).map(
+						name: workspaceFolder.name,
+						objects: glob.sync(`${workspaceFolder.uri.path.slice(1)}/**/*`).map(
 							(_) => {
 								let r = {
 									path: _.replace(/\\/g, "/").slice(workspaceFolder.uri.path.slice(1).length),
@@ -57,86 +182,81 @@ class Server {
 						),
 					}
 				});
-				response.writeHead(200, {
-					"Content-Type": "application/json"
-				});
-				response.end(JSON.stringify(workspaceItems), "utf-8");
+				return { workspace: workspace };
 			},
-			"/getFile.get": (query, response) => {
+			"GetFilePath": (data) => {
 				let workspaceFolders = {};
 				vscode.workspace.workspaceFolders.forEach((workspaceFolder) => {
 					workspaceFolders[workspaceFolder.name] = workspaceFolder.uri.path;
 				});
-				query = JSON.parse(query);
-				fs.readFile(`${workspaceFolders[query.workspace].slice(1)}${query.path}`,
-					(error, content) => {
-						response.writeHead(200, { "Content-Type": "text/javascript" });
-						response.end(content, "utf-8");
-					}
-				);
+				return { filePath: `${workspaceFolders[data.workspace].slice(1)}${data.path}` };
 			}
 		};
-		this.httpServer = http.createServer((request, response) => {
-			if (url.parse(request.url).pathname in this.API) {
-				let R = this.API[url.parse(request.url).pathname](decodeURI(url.parse(request.url).query), response);
+	}
 
-			} else {
-				fs.readFile(this.context.asAbsolutePath("/media/index.html"),
-					(error, content) => {
-						response.writeHead(200, { "Content-Type": "text/html" });
-						response.end(content, "utf-8");
-					});
-			}
-		}).listen(7777);
+	start() {
+		this.webSocketClient = new ws("http://127.0.0.1:8887");
+		this.webSocketClient.addEventListener("open", (event) => {
 
-		this.webSocketServer = new ws.Server({ port: 8888 });
-		this.webSocketServer.on("connection", webSocket => {
-			webSocket.on("message", (message) => {
-			});
-			webSocket.on("close", () => {
-			});
+		});
+		this.webSocketClient.addEventListener("message", (event) => {
+			let data = JSON.parse(event.data);
+			let R = this.API[data.type](data);
+			delete data.type;
+			this.webSocketClient.send(
+				JSON.stringify(Object.assign(data, R))
+			);
+		});
+
+		this.webSocketClient.addEventListener("close", (event) => {
+
+		});
+
+		this.webSocketClient.addEventListener("error", (event) => {
+
 		});
 		this.OnLine = true;
 	}
-
 	stop() {
-		this.httpServer.close();
-		this.webSocketServer.close();
+		this.webSocketClient.close();
 		this.OnLine = false;
 	}
 }
+
+let state;
+let server;
+let gate;
 
 /**
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
-	let ui = new UI();
-	ui.init();
-	let server = new Server(context);
+	gate = new Gate(context);
+	gate.start();
+	server = new Server(context);
+	state = new State(context);
+	state.init();
+	state.offLine();
 
 	Object.entries(
 		{
 			"BlueFoxServer.OnLine": () => {
 				if (!server.OnLine) {
 					server.start();
-					ui.statusBarItem.text = "^.,.^ BlueFox is OnLine.";
-					ui.statusBarItem.command = "BlueFoxServer.OffLine";
+					state.onLine();
 				}
 			},
 			"BlueFoxServer.OffLine": () => {
 				if (server.OnLine) {
 					server.stop();
-					ui.statusBarItem.text = "-.,.^ BlueFox is OffLine.";
-					ui.statusBarItem.command = "BlueFoxServer.OnLine";
+					state.offLine();
 				}
 			},
 			"BlueFoxServer.RunScript": (_) => {
 				if (server.OnLine) {
-					ui.statusBarItem.text = "^.,.^ BlueFox is OnLine.";
-					ui.statusBarItem.command = "BlueFoxServer.OffLine";
-					fs.readFile(_.path.slice(1),"utf-8",
+					fs.readFile(_.path.slice(1), "utf-8",
 						(error, content) => {
-							[...server.webSocketServer.clients][0].send(
+							[...gate.webSocketServer.clients][0].send(
 								JSON.stringify(
 									{
 										type: "RunScript",
@@ -147,31 +267,27 @@ function activate(context) {
 						});
 				} else {
 					server.start();
-					ui.statusBarItem.text = "^.,.^ BlueFox is OnLine.";
-					ui.statusBarItem.command = "BlueFoxServer.OffLine";
-					fs.readFile(_.path.slice(1),"utf-8",
-					(error, content) => {
-						[...server.webSocketServer.clients][0].send(
-							JSON.stringify(
-								{
-									type: "RunScript",
-									content: content,
-								}
-							)
-						);
-					});
+					state.onLine();
+					fs.readFile(_.path.slice(1), "utf-8",
+						(error, content) => {
+							[...gate.webSocketServer.clients][0].send(
+								JSON.stringify(
+									{
+										type: "RunScript",
+										content: content,
+									}
+								)
+							);
+						});
 				}
 			},
 			"BlueFoxServer.OpenBrowser": () => {
 				if (server.OnLine) {
 					opn("http://127.0.0.1:7777");
-					ui.statusBarItem.text = "^.,.^ BlueFox is OnLine.";
-					ui.statusBarItem.command = "BlueFoxServer.OffLine";
 				} else {
 					server.start();
+					state.onLine();
 					opn("http://127.0.0.1:7777");
-					ui.statusBarItem.text = "^.,.^ BlueFox is OnLine.";
-					ui.statusBarItem.command = "BlueFoxServer.OffLine";
 				}
 			},
 		}
@@ -184,7 +300,10 @@ function activate(context) {
 
 }
 
-function deactivate() { }
+function deactivate() {
+	state.offLine();
+	server.stop();
+}
 
 module.exports = {
 	activate,
